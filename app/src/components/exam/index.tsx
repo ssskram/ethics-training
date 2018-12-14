cd0068c84cc6e181f4be7bdc47e9d986939b73fe
ssskram <paul.marks@pittsburghpa.gov>
2018-12-14 17:20:28 -0500

    folder structure, scaffolding

--- app/src/components/exam/index.tsx
+++ app/src/components/exam/index.tsx
@@ -3,6 +3,12 @@ import * as React from 'react'
 
 export default class Exam extends React.Component<any, any> {
 
+    // when next button is clicked, post highpoint change
+    // when back button is click, default answer to correct if question is less than highpoint
+    // when closed and picked back up, select question by high point ID
+    // if next module != this module, throw encouraging modal...or something?
+    // parent 
+
     public render() {
 
         return (
